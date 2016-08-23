28238f16daa5fe9f33d90a7932519885405c30cd
Joe Phearse <jphearse@gmail.com>
2016-08-23 14:41:11 -0500

    removed commented out code

--- public/index.php
+++ public/index.php
@@ -17,16 +17,6 @@
     <link href="/css/welcome.css" rel="stylesheet">
     <!-- <link href="/css/font.css" rel="stylesheet"> -->
    
-
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -69,16 +59,6 @@
 	    </div> <!-- /container -->
 	</div><!--/headerwrap -->
 	
-	<div id="aboutwrap">
-	    <div class="container">
-			<div class="row">
-				<div class="col-lg-6 col-lg-offset-3">
-
-				</div>
-			</div><!--/row -->
-	    </div> <!-- /container -->
-	</div><!--/aboutwrap -->
-
 
 	<!--Resume-->	
 	<section id="about">
@@ -273,9 +253,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="/js/bootstrap.js" ></script>
