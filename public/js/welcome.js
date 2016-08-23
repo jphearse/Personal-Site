739cad06c81e2ec1438fe681ec2b223ca73a4c59
Joe Phearse <jphearse@gmail.com>
2016-08-23 09:44:30 -0500

    added animation to header

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,11 +1,11 @@
 (function() {
-$(".headContents").css("margin-top", "8px");
-$(".headContents").css("opacity", "0");
-rise();
-// function fade(){
-// 	$(".headContents").hide().fadeIn(1500);
-// }
-function rise(){
-	$(".headContents").animate({"opacity":"1","margin-top":"-=15px"}, 800);
-}
+	$(".headContents").css("margin-top", "30px");
+	$(".headContents").css("opacity", "0");
+	rise();
+	// function fade(){
+	// 	$(".headContents").hide().fadeIn(1500);
+	// }
+	function rise(){
+		$(".headContents").animate({"opacity":"1","margin-top":"-=30px"}, 1000);
+	}
 })();
