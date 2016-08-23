57c3c8da17487a97fdd47cc2215ce11f5387d717
Joe Phearse <jphearse@gmail.com>
2016-08-23 10:08:43 -0500

    changed file name

---
+++ public/index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <title>Joe Phearse</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/welcome.css" rel="stylesheet">
+    <!-- <link href="/css/font.css" rel="stylesheet"> -->
+   
+
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+<div class="container-fixed">
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#headerwrap">Joe Phearse</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+       
+        <li><a href="#headerwrap">Home</a></li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#works">Portfolio</a></li>
+       
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
+</div>
+
+	<div id="headerwrap">
+	    <div class="container">
+			<div class="row">
+				<div class="headContents col-lg-6 col-lg-offset-3">
+					<div class="avatar">
+                    	<img alt="" src=/img/headshot.png>
+                	</div>
+					<h1 class="about">Joe Phearse</h1>
+					<a href="mailto:jphearse@gmail.com" target="_top"><h3 class="about">Jphearse@gmail.com</h3></a>
+				</div>
+			</div><!--/row -->
+	    </div> <!-- /container -->
+	</div><!--/headerwrap -->
+	
+	<div id="aboutwrap">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3">
+
+				</div>
+			</div><!--/row -->
+	    </div> <!-- /container -->
+	</div><!--/aboutwrap -->
+
+
+	<!--Resume-->	
+	<section id="about">
+	<div class="container">
+		<div class="row centered mt mb">
+			<div class="col-lg-8 col-lg-offset-2">
+
+				<h1>About Me</h1>
+
+				<div>
+					<img id="pic-of-me" src="/img/collage.jpg">	<br><br>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+
+				<br>
+
+				<h2>Education</h2><hr>
+				<p><strong>The University of Texas at San Antonio</strong></strong></p>
+				<p><em><strong>San Antonio, Texas</strong> </em></p>
+				<div id="textbox">
+
+					<p class="alignleft1"><strong>M.A. Education,</strong></p>
+				
+					<p class="alignright1">August 2015 - Present</p>
+
+				</div>
+				
+				<div style="clear: both;"></div>
+
+				<div class="alignleft1 indent"><em>-Instructional Technology</em></div><br><br>
+
+				<div id="textbox">
+				
+					<p class="alignleft1"><strong>B.A. Economics</strong></p>
+				
+				
+					<p class="alignright1">May 2014 - August 2015</p>
+					
+				</div>
+				<div style="clear: both;"></div>
+				<p><strong>The University of Houston</strong></p>
+				<p><em><strong>Houston, Texas</strong> </em></p>
+				<div id="textbox">
+				
+					<p class="alignleft1">Computer Science</p>
+					
+					
+					<p class="alignright1">August 2012 - May 2014</p>
+					</div>
+				
+				<div style="clear: both;"></div>
+
+				<h2>Work Experience</h2><hr>
+		
+				<div id="textbox">
+					<p class="alignleft1"><strong>Lead Inventory</strong></p>
+					<p class="alignright1">August 2014 - July 2016</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>Carmax, San Antonio, Texas</em></p>
+				<ul>
+					<li>Executes quality control checks on any received/expected inventory</li>
+					<li>Innovates beneficial facility and ground maintenance techniques to enhance inventory quality</li>
+					<li>Maintains computerized records of tracking inventory</li>
+					<li>Communicates with staff members storewide to better serve customers</li>
+				</ul>
+				<div id="textbox">
+					<p class="alignleft1 col-sm-8 col-xs-6"><strong>Guest Services Representative</strong></p>
+					<p class="alignright1 col-sm-4 col-xs-4">September 2011 - August 2012</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>Westwood Cinema 6, Brenham, Texas</em></p>
+				<ul>
+					<li>Collected/sold tickets to customers</li>
+					<li>Assisted guests with complaints and special requests</li>
+					<li>Ushered and maintained customer orderliness during peak hours</li>
+					<li>Provided comfortable atmosphere for guests through sociability and cleanliness</li>
+				</ul>
+				<div id="textbox">
+					<p class="alignleft1 col-sm-8 col-xs-6"><strong>Power Supply Unit Tester Intern</strong></p>
+					<p class="alignright1 col-sm-4 col-xs-4">April 2011 - August 2011</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="job-location"><em>HDL Research Lab, Brenham, Texas</em></p>
+				<ul>
+					<li>Tested unitary components of power supply technology through electric simulation</li>
+					<li>Ensured that products met military standards</li>
+					<li>Interacted with professionals across various departments to maintain the checks and balances of the
+					company</li>
+				</ul>
+
+				<h2>Recognition</h2><hr>
+				<div id="textbox">
+					<p class="alignleft1"><strong>Dean's List</strong></p>
+					<p class="alignright1">Summer 2014</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="recog"><em>University of Texas at San Antonio, San Antonio, Texas</em></p>
+				<div id="textbox">
+					<p class="alignleft1"><strong>Store Champion (Employee of the Month)</strong></p>
+					<p class="alignright1">April 2015, February 2016</p>
+				</div>
+				<div style="clear: both;"></div>
+				<p class="recog"><em>Carmax, San Antonio, Texas</em></p>
+
+
+
+				<h2>Skills</h2><hr>
+				<ul>
+					<li><strong>Computer:</strong> experienced with HTML, CSS, JavaScript, Apache Subversion (SVN), SCORM, and GIMP, proficient in Microsoft Office</li>
+					<li><strong>Problem Solving:</strong> experience working with deadlines and managing multiple, demanding tasks as well as creating innovative and efficient solutions</li>
+					<li><strong>Social Engagement:</strong> able to communicate verbally and in writing in a positive manner with colleagues and clientele</li>
+					<li><strong>Work Ethic:</strong> detail-oriented in both straightforward and complex tasks, enjoys challenging and engaging work, capable of taking initiative as well as following orders and working without supervision, adaptable in new environments</li>
+				</ul>
+
+			</div>
+
+
+
+
+		</div><!--row -->
+	</div><!--container -->
+	</section>
+<!--Resume-->
+
+		
+	<section id="works">
+	<div class="container">
+		<div class="row centered mt mb">
+			<h1>My Portfolio</h1>
+			
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/calculator.html" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/weather_map.html" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/index-simple.html" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/pizza_form.php" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/pizza_form.php" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 gallery">
+				<a href="/pizza_form.php" target="_blank"><img src="/img/pizza-slice.jpg" class="img-responsive"></a>
+			</div>
+
+		</div><!--/row -->
+	</div><!--/container -->
+	</section>
+
+<div id="social">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-3">
+					<a href="https://github.com/jphearse" target="_blank"><i class="fa fa-github"></i></a>
+				</div>
+				<div class="col-lg-3">
+					<a href="https://www.facebook.com/joe.phearse" target="_blank"><i class="fa fa-facebook"></i></a>
+				</div>
+				
+				<div class="col-lg-3">
+					<a href="https://www.linkedin.com/in/joe-phearse-70162bbb?trk=nav_responsive_tab_profile" target="_blank"><i class="fa fa-linkedin"></i></a>
+				</div>
+				<div class="col-lg-3">
+					<a href="https://www.instagram.com/jphearse/?hl=en" target="_blank"><i class="fa fa-instagram" ></i></a>
+				</div>
+				
+			
+			</div><!--/row -->
+		</div><!--/container -->
+	</div><!--/social -->
+
+	<div id="footerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-4">
+					<p><b>Codeup Student</b></p>
+				</div>
+			
+				<div class="col-lg-4">
+					<p>San Antonio</p>
+				</div>
+				<div class="col-lg-4">
+					<p>Jphearse@gmail.com</p>
+				</div>
+			</div>
+		</div>
+	</div><!--/footerwrap -->
+	
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="/js/bootstrap.js" ></script>
+	<script src="js/welcome.js"></script>
+  </body>
+</html>
