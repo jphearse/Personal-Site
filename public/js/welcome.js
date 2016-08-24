d352a75b1a7090fc9a4c48e2e3899cdf685f4ae8
Joe Phearse <jphearse@gmail.com>
2016-08-24 14:32:46 -0500

    removed code for scrolling

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -12,12 +12,6 @@
 		$(".headContents").animate({"opacity":"1","margin-top":"-=30px"}, 1000);
 	}
 
-
-	$(window).scroll(function() {
-  var scrolledY = $(window).scrollTop();
-  $('#headwrap').css('background-position', 'left ' + ((scrolledY)) + 'px');
-});
-
 ///////////////smooth scrolling//////////////////////////////////////////
 
 	$('#nameLink').click(function(){
