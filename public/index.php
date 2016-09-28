b9033e682126eb757494b6795def057bf8b02d02
Joe Phearse <jphearse@gmail.com>
2016-09-28 09:04:37 -0500

    added meta tags

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Joe Phearse Personal Site">
+    <meta name="author" content="Joe Phearse">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 
     <title>Joe Phearse</title>
